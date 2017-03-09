2afc5e8f618db786e0f0a90c629bfb20fbd467f6
Marcelo Ponciano <marceloponciano.eb@gmail.com>
2017-03-09 01:02:51 -0300

    Primeiro Commit

---
+++ src/main/java/br/mil/eb/caddti/CadastroDtiApplication.java
@@ -0,0 +1,12 @@
+package br.mil.eb.caddti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDtiApplication.class, args);
+	}
+}
